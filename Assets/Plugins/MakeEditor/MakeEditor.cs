197560d31ce36b7122ef87b1dc402d3effb3e1cb
Krzysztof Begiedza <krzysztof.begiedza@gmail.com>
2019-07-07 09:31:45 +0200

    Make editor init

---
+++ Assets/Plugins/MakeEditor/MakeEditor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Bloodstone.MakeEditor
+{
+    public class MakeEditor : MonoBehaviour
+    {
+        [MenuItem("Assets/Create/Editor script")]
+        public static void CreateEditorScript()
+        {
+            var selected = Selection.objects;
+            if(selected.Length > 1)
+            {
+                //create standard editor or for each selected?
+            }
+            else if (selected.Length == 1)
+            {
+                var o = selected[0];
+                var path = AssetDatabase.GetAssetPath(o);
+
+                print($"Editor : {path}");
+            }
+        }
+
+
+        [MenuItem("Assets/Create/PropertyDrawer")]
+        public static void CreatePropertyDrawer()
+        {
+            print($"PropertyDrawer!");
+        }
+    }
+}
