1fb465316f9623e977e9408ed717be7611bf6973
Krzysztof Begiedza <krzysztof.begiedza@gmail.com>
2019-07-07 11:47:23 +0200

    Added make editor script extension

--- Assets/Plugins/MakeEditor/MakeEditor.cs
+++ Assets/Plugins/MakeEditor/MakeEditor.cs
@@ -1,10 +1,25 @@
-﻿using UnityEditor;
+﻿using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEditor;
 using UnityEngine;
 
 namespace Bloodstone.MakeEditor
 {
-    public class MakeEditor : MonoBehaviour
+    [InitializeOnLoad]
+    public class MakeEditor
     {
+        private static string _editorTemplate;
+        
+        static MakeEditor()
+        {
+            var pluginAsmdef = AssetDatabase.FindAssets($"t:asmdef Bloodstone.MakeEditor")[0];
+            var pluginPath = AssetDatabase.GUIDToAssetPath(pluginAsmdef);
+
+            _editorTemplate = Path.Combine(Path.GetDirectoryName(pluginPath), "editor_template.txt");
+            Debug.Log(_editorTemplate);
+        }
+
         [MenuItem("Assets/Create/Editor script")]
         public static void CreateEditorScript()
         {
@@ -15,18 +30,56 @@ namespace Bloodstone.MakeEditor
             }
             else if (selected.Length == 1)
             {
-                var o = selected[0];
-                var path = AssetDatabase.GetAssetPath(o);
+                var firstSelected = selected[0];
+                var firstSelectedPath = AssetDatabase.GetAssetPath(firstSelected);
+                Debug.Log($"first Selected path: {firstSelectedPath}");
+                var ms = AssetDatabase.LoadAssetAtPath<MonoScript>(firstSelectedPath);
 
-                print($"Editor : {path}");
+                var dirPath = Path.GetDirectoryName(firstSelectedPath);
+                var path = Path.Combine(dirPath, "Editor" , $"{firstSelected.name}Editor.cs");
+
+
+                //should override?
+                if (File.Exists(path))
+                {
+                    //
+                }
+                else
+                {
+                    Directory.CreateDirectory(Path.GetDirectoryName(path));
+                    
+                    //File.Copy(_editorTemplate, path);
+                    var content = PrepareContent(_editorTemplate, firstSelectedPath, ms);
+                    File.WriteAllText(path, content);
+                    AssetDatabase.Refresh();
+                }
             }
         }
 
-
-        [MenuItem("Assets/Create/PropertyDrawer")]
-        public static void CreatePropertyDrawer()
+        private static string PrepareContent(string template, string target, MonoScript s)
         {
-            print($"PropertyDrawer!");
+            var code = File.ReadAllLines(template).ToList();
+
+            var type = s.GetClass();
+            if(type.Namespace != null)
+            {
+                //namespace
+                for (int i = 0; i < code.Count; ++i)
+                {
+                    code[i] = Regex.Replace(code[i], "#NAMESPACE#", type.Namespace);
+                    code[i] = Regex.Replace(code[i], "#CLASS_NAME#", type.Name);
+                }
+            }
+
+            //namespace
+            for (int i = 0; i < code.Count; ++i)
+            {
+                code[i] = Regex.Replace(code[i], "#CLASS_NAME#", type.Name);
+            }
+
+            var finalCode = string.Join("\n", code.ToArray());
+
+            return finalCode;
         }
     }
 }
