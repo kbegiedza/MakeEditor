73818a161392416de4e3e0b363da9fa346165f82
Krzysztof Begiedza <krzysztof.begiedza@gmail.com>
2019-07-18 19:36:35 +0200

    Moved class to separate file

--- Assets/Plugins/MakeEditor/Editor/MakeEditor.cs
+++ Assets/Plugins/MakeEditor/Editor/MakeEditor.cs
@@ -9,7 +9,6 @@ using UnityObject = UnityEngine.Object;
 
 namespace Bloodstone.MakeEditor
 {
-
     [InitializeOnLoad]
     public static class MakeEditor
     {
@@ -51,17 +50,16 @@ namespace Bloodstone.MakeEditor
 
             if (selectedScripts.Length > 0)
             {
-                using (var reloadLock = new ReloadAssembliesLock())
+                using (new ReloadAssembliesLock())
                 {
                     UnityObject lastCreatedObject = null;
                     var editorScriptTemplate = File.ReadAllLines(_editorTemplatePath);
 
                     foreach (var selectedScript in selectedScripts)
                     {
-                        var selectedScriptPath = AssetDatabase.GetAssetPath(selectedScript);
-
                         //deep copy to prevent template modifications
                         var newScriptCode = editorScriptTemplate.ToList();
+                        var selectedScriptPath = AssetDatabase.GetAssetPath(selectedScript);
 
                         lastCreatedObject = CreateScriptAsset(newScriptCode, selectedScriptPath);
                     }
@@ -89,9 +87,8 @@ namespace Bloodstone.MakeEditor
                 Directory.CreateDirectory(requiredDirectory);
 
                 File.WriteAllText(outputPath, scriptContent);
-                AssetDatabase.Refresh();
-
                 CreateAssembly(asmPath, outputPath);
+
                 AssetDatabase.Refresh();
 
                 return AssetDatabase.LoadAssetAtPath(outputPath, typeof(UnityObject));
@@ -193,54 +190,6 @@ namespace Bloodstone.MakeEditor
             var finalCode = string.Join("\n", scriptCode.ToArray());
 
             return finalCode;
-        }
-
-        private static class PathUtility
-        {
-            private const string _scriptExtension = ".cs";
-            private const string _templateFolder = "Templates";
-            private const string _editorTemplateName = "editor_template.txt";
-            private const string _pluginAssemblyFilter = "t:asmdef Bloodstone.MakeEditor";
-
-            public static string GetScriptPath(string rootPath, string subjectPath)
-            {
-                var editorPath = Path.Combine(rootPath, "Editor");
-                var pathMod = Path.GetDirectoryName(subjectPath.Substring(rootPath.Length + 1)); //+1 to remove '/'
-
-                var dirPath = Path.Combine(editorPath, pathMod);
-
-                var name = Path.GetFileNameWithoutExtension(subjectPath);
-                var outputPath = Path.Combine(dirPath, $"{name}Editor.cs");
-
-                if (File.Exists(outputPath))
-                {
-                    outputPath = GenerateNotExistingFilename(outputPath);
-                }
-
-                return outputPath;
-            }
-
-            public static string FindEditorTemplatePath()
-            {
-                var guids = AssetDatabase.FindAssets(_pluginAssemblyFilter);
-                if (guids.Length <= 0)
-                {
-                    throw new FileNotFoundException("Cannot find Bloodstone.MakeEditor assembly definition");
-                }
-
-                var pluginPath = Path.GetDirectoryName(AssetDatabase.GUIDToAssetPath(guids[0]));
-                return Path.Combine(pluginPath, _templateFolder, _editorTemplateName);
-            }
-
-            private static string GenerateNotExistingFilename(in string path)
-            {
-                var directory = Path.GetDirectoryName(path);
-                var fileName = Path.GetFileNameWithoutExtension(path);
-
-                var newFileName = fileName + _scriptExtension;
-
-                return Path.Combine(directory, newFileName);
-            }
         }
 
         private class ReloadAssembliesLock : IDisposable
---
+++ Assets/Plugins/MakeEditor/Editor/PathUtility.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using UnityEditor;
+
+namespace Bloodstone.MakeEditor
+{
+    public static class PathUtility
+    {
+        private const string _scriptExtension = ".cs";
+        private const string _templateFolder = "Templates";
+        private const string _editorTemplateName = "editor_template.txt";
+        private const string _pluginAssemblyFilter = "t:asmdef Bloodstone.MakeEditor";
+
+        public static string GetScriptPath(string rootPath, string subjectPath)
+        {
+            var editorPath = Path.Combine(rootPath, "Editor");
+            var pathMod = Path.GetDirectoryName(subjectPath.Substring(rootPath.Length + 1)); //+1 to remove '/'
+
+            var dirPath = Path.Combine(editorPath, pathMod);
+
+            var name = Path.GetFileNameWithoutExtension(subjectPath);
+            var outputPath = Path.Combine(dirPath, $"{name}Editor.cs");
+
+            if (File.Exists(outputPath))
+            {
+                outputPath = GenerateNotExistingFilename(outputPath);
+            }
+
+            return outputPath;
+        }
+
+        public static string FindEditorTemplatePath()
+        {
+            var guids = AssetDatabase.FindAssets(_pluginAssemblyFilter);
+            if (guids.Length <= 0)
+            {
+                throw new FileNotFoundException("Cannot find Bloodstone.MakeEditor assembly definition");
+            }
+
+            var pluginPath = Path.GetDirectoryName(AssetDatabase.GUIDToAssetPath(guids[0]));
+            return Path.Combine(pluginPath, _templateFolder, _editorTemplateName);
+        }
+
+        private static string GenerateNotExistingFilename(in string path)
+        {
+            var directory = Path.GetDirectoryName(path);
+            var fileName = Path.GetFileNameWithoutExtension(path);
+
+            var newFileName = fileName + _scriptExtension;
+
+            return Path.Combine(directory, newFileName);
+        }
+    }
+}
